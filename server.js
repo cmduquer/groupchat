2b62ac188ee0fb0f8c5d9d6f299697a96eddc2f7
cmduquer <cmduquer@gmail.com>
2017-03-17 10:15:26 -0500

    Test

--- server.js
+++ server.js
@@ -17,6 +17,19 @@ io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
   socket.on('message', (socket) => io.sockets.emit('updatechat', socket, socket));
+  socket.on('adduser', function(username){
+		// we store the username in the socket session for this client
+		socket.username = username;
+		// add the client's username to the global list
+		usernames[username] = username;
+		// echo to client they've connected
+		socket.emit('updatechat', 'SERVER', 'you have connected');
+		// echo globally (all clients) that a person has connected
+		socket.broadcast.emit('updatechat', 'SERVER', username + ' has connected');
+		// update the list of users in chat, client-side
+		io.sockets.emit('updateusers', usernames);
+	});
 });
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+
